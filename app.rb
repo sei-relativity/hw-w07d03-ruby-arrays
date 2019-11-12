b4f593ca9c55fb5e9b6e658b84066748968ad23b
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-11-12 18:40:29 +0300

    added my solutions (not completed yet)

--- app.rb
+++ app.rb
@@ -88,92 +88,95 @@
 # # {:description=>"bees"}
 # # {:description=>"a MacBook"}
 
-# #____________________ 3. Find the average amount spent on coffee, per transaction, for each person
-# people = [
-#   {
-#     name: 'Sarah',
-#     transactions: [
-#       {
-#         type: 'COFFEE',
-#         amount: 7.43
-#       },
-#       {
-#         type: 'TACOS',
-#         amount: 14.65
-#       },
-#       {
-#         type: 'COFFEE',
-#         amount: 4.43
-#       }
-#     ]
-#   },
-#   {
-#     name: 'Saud',
-#     transactions: [
-#       {
-#         type: 'BIKES',
-#         amount: 800.00
-#       },
-#       {
-#         type: 'TACOS',
-#         amount: 14.65
-#       },
-#       {
-#         type: 'COFFEE',
-#         amount: 4.43
-#       }
-#     ]
-#   },
-#   {
-#     name: 'Norah',
-#     transactions: [
-#       {
-#         type: 'COFFEE',
-#         amount: 7.43
-#       },
-#       {
-#         type: 'COFFEE',
-#         amount: 100.00
-#       },
-#       {
-#         type: 'COFFEE',
-#         amount: 4.43
-#       }
-#     ]
-#   }
-# ]
+#____________________ 3. Find the average amount spent on coffee, per transaction, for each person
+people = [
+  {
+    name: 'Sarah',
+    transactions: [
+      {
+        type: 'COFFEE',
+        amount: 7.43
+      },
+      {
+        type: 'TACOS',
+        amount: 14.65
+      },
+      {
+        type: 'COFFEE',
+        amount: 4.43
+      }
+    ]
+  },
+  {
+    name: 'Saud',
+    transactions: [
+      {
+        type: 'BIKES',
+        amount: 800.00
+      },
+      {
+        type: 'TACOS',
+        amount: 14.65
+      },
+      {
+        type: 'COFFEE',
+        amount: 4.43
+      }
+    ]
+  },
+  {
+    name: 'Norah',
+    transactions: [
+      {
+        type: 'COFFEE',
+        amount: 7.43
+      },
+      {
+        type: 'COFFEE',
+        amount: 100.00
+      },
+      {
+        type: 'COFFEE',
+        amount: 4.43
+      }
+    ]
+  }
+]
 
-# coffee_average_per_person = []
+coffee_average_per_person = []
 
-# coffee_average_per_person = people.map do |person|
+coffee_average_per_person = people.map do |person|
 
-#   coffee_average = 0
-#   coffee_count  = 0
-#   total_transactions_amount = 0
+  coffee_average     = 0
+  coffee_count       = 0
+  total_transactions = 0
 
-#   person[:transactions].each do |transaction|
-#     if transaction[:type] == "COFFEE"
-#       total_transactions_amount += transaction[:amount]
-#       coffee_count += 1
-#       # puts "\ntotal_transactions_amount: #{total_transactions_amount} inside each"
-#       # puts "coffee_count: #{coffee_count} inside each"
-#     end
-#   end
+  person[:transactions].each do |transaction|
+    if transaction[:type] == "COFFEE"
+      total_transactions += transaction[:amount]
+      coffee_count += 1
+      # puts "\ntotal_transactions_amount: #{total_transactions} inside each"
+      # puts "coffee_count: #{coffee_count} inside each"
+    end
+  end
 
-#   # puts "_______________\ntotal_transactions_amount: #{total_transactions_amount} outside each"
-#   # puts "coffee_count: #{coffee_count} outside each"
+  # puts "_______________\ntotal_transactions_amount: #{total_transactions} outside each"
+  # puts "coffee_count: #{coffee_count} outside each"
 
-#   coffee_average = total_transactions_amount / coffee_count
+  coffee_average = total_transactions / coffee_count
 
-#   {name:person[:name], coffe_average: coffee_average}
-# end
+  {
+    name:person[:name],
+    coffee_average: coffee_average
+  }
+end
 
-# puts coffee_average_per_person
+puts coffee_average_per_person
 
-# # Answer
-# # {:name=>"Sarah", :coffee_average=>5.93}
-# # {:name=>"Saud", :coffee_average=>4.43}
-# # {:name=>"Norah", :coffee_average=>37.28666666666667}
+# Answer
+# {:name=>"Sarah", :coffee_average=>5.93}
+# {:name=>"Saud", :coffee_average=>4.43}
+# {:name=>"Norah", :coffee_average=>37.28666666666667}
 
 # #____________________ 4. Find the most expensive product for each store, with the store name:
 # stores = [
