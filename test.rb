a67dfa1b622c3c07981010a477e031b3b008922f
noura <no_-_no@hotmail.com>
2019-11-12 20:16:03 +0300

    My solve

--- test.rb
+++ test.rb
@@ -1,11 +1,4 @@
-# Ruby Map Practice
-
-![ARRAYS](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWBMdo6Ac3moY3tPnzMsFVnOscOR03SxkZ4sPGGhsWoQrYMPZ9g)
-
-## 1. Return an array of each Student's full name, upper-cased
-
-```rb
-
+#1
 students = [
   {
       first_name: 'Lamees',
@@ -23,25 +16,14 @@ students = [
 
 upper_case_full_names = []
 
-```
-
-### Answer
-
  students.map do |student|
     upper_case_full_names.push(("#{student[:first_name]} #{student[:last_name]}").upcase)
  end
  p upper_case_full_names
 
-```rb
-LAMEES ALFALLAJ
-AISHA ALDABBAGH
-ABDULWAHHAB ALBALLA
-```
+###################################################
 
-## 2. Find the first order for each user
-
-```rb
-
+#2
 users = [
   {
       name: 'Fahad',
@@ -81,11 +63,7 @@ users = [
   }
 ]
 
-first_order_for_each_user = []
-
-```
-
-### Answer
+  first_order_for_each_user = []
 
   users.each do |user| 
     first_order_for_each_user.push(user[:orders].first)
@@ -93,17 +71,10 @@ first_order_for_each_user = []
 
 p first_order_for_each_user
 
-```rb
 
-{:description=>"a bike"}
-{:description=>"bees"}
-{:description=>"a MacBook"}
+###################################################
 
-```
-
-## 3. Find the average amount spent on coffee, per transaction, for each person
-
-```rb
+#3
 
 people = [
   {
@@ -159,12 +130,8 @@ people = [
   }
 ]
 
-
 coffee_average_per_person = []
 
-```
-
-### Answer
   people.each{|person| 
     coffee = 0
     sum = 0
@@ -179,17 +146,10 @@ coffee_average_per_person = []
 }
 p coffee_average_per_person
 
-```rb
 
-{:name=>"Sarah", :coffee_average=>5.93}
-{:name=>"Saud", :coffee_average=>4.43}
-{:name=>"Norah", :coffee_average=>37.28666666666667}
+###################################################
 
-```
-
-## 4. Find the most expensive product for each store, with the store name:
-
-```rb
+#4
 
 stores = [
   {
@@ -235,9 +195,6 @@ stores = [
 
 most_expensive_products_by_store = []
 
-```
-
-### Answer
 most_expensive_products_by_store = stores.map do |store|
     most_price=0
     item =''
@@ -252,31 +209,4 @@ most_expensive_products_by_store = stores.map do |store|
 
       p most_expensive_products_by_store
 
-
-```rb
-
-{:store_name=>"Jarir", :most_expensive_product=>{:description=>"Titanium", :price=>9384.33}}
-{:store_name=>"Tamimi", :most_expensive_product=>{:description=>"Silver", :price=>654.44}}
-{:store_name=>"Souq", :most_expensive_product=>{:description=>"Sapphire", :price=>899.33}}
-
-```
-
-# Bonus
-
-Write an infinite loop that will make you add all the your friends in the students list and after each add will ask if you want to quit the loop (yes/no) if the user choose no print all the students array
-
-### Answer
-
-```
-
-add a student
-Mohammed AlOfaysan
-Do you want to continue ? (y/n)
-y
-add a student
-Raje AlHarthi
-Do you want to continue ? (y/n)
-y
-add a student
-
-```
+     
