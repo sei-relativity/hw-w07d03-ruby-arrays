b63ef4c87822d0249db71dd3f9d016c33fffedf9
Muhannad Alanazi <muhannad.h.alanazi@gmail.com>
2019-11-12 18:45:52 +0300

    done

--- app.rb
+++ app.rb
@@ -131,64 +131,68 @@
 
 #   end
 #  p coffee_average_per_person
-# stores = [
-#     {
-#         store_name: 'Jarir',
-#         products: [
-#             {
-#                 description: 'Titanium',
-#                 price: 9384.33
-#             },
-#             {
-#                 description: 'Gold',
-#                 price: 345.54
-#             }
-#         ]
-#     },
-#     {
-#         store_name: 'Tamimi',
-#         products: [
-#             {
-#                 description: 'Silver',
-#                 price: 654.44
-#             },
-#             {
-#                 description: 'Ruby',
-#                 price: 323.43
-#             }
-#         ]
-#     },
-#     {
-#         store_name: 'Souq',
-#         products: [
-#             {
-#                 description: 'Opal',
-#                 price: 345.43
-#             },
-#             {
-#                 description: 'Sapphire',
-#                 price: 899.33
-#             }
-#         ]
-#     }
-#   ]
+stores = [
+    {
+        store_name: 'Jarir',
+        products: [
+            {
+                description: 'Titanium',
+                price: 9384.33
+            },
+            {
+                description: 'Gold',
+                price: 345.54
+            }
+        ]
+    },
+    {
+        store_name: 'Tamimi',
+        products: [
+            {
+                description: 'Silver',
+                price: 654.44
+            },
+            {
+                description: 'Ruby',
+                price: 323.43
+            }
+        ]
+    },
+    {
+        store_name: 'Souq',
+        products: [
+            {
+                description: 'Opal',
+                price: 345.43
+            },
+            {
+                description: 'Sapphire',
+                price: 899.33
+            }
+        ]
+    }
+  ]
   
-#   most_expensive_products_by_store = []
-#   stores.each do |index|
-#     if index[:products][0][:price]>index[:products][1][:price]
-#         most_expensive_products_by_store.push({store_name: index[:store_name], most_expensive_products: index[:products][0]})
-#     else
-#         most_expensive_products_by_store.push({store_name: index[:store_name], most_expensive_products: index[:products][1]})
-#     end
+  most_expensive_products_by_store = []
+  stores.each do |index|
+    highest_price = 0;
+    item_desrciption = '';
+    index[:products].each do |product|
+        if product[:price] > highest_price
+            highest_price = product[:price]
+            item_desrciption = product[:description]
+        end
+    end
+        most_expensive_products_by_store.push({store_name: index[:store_name], most_expensive_products: {description: item_desrciption, price: highest_price}})
+    end
     
-#   end
-#   p most_expensive_products_by_store
-input = ""
-students = []
-while input != "n" do
-  puts "Write Student name"
-  students.push(gets.chomp)
-  puts "Would you like to add a student? (y/n)"
-  input = gets.chomp
-end
-p students
+  p most_expensive_products_by_store
+# input = ""
+# students = []
+# while input != "n" do
+#   puts "Write Student name"
+#   students.push(gets.chomp)
+#   puts "Would you like to add a student? (y/n)"
+#   input = gets.chomp
+# end
+# p students
