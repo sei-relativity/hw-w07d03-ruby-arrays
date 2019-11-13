fd7fe05953edf03a25dc631dbec94f673a21300c
aba20 <askr1000@gmail.com>
2019-11-13 08:07:06 +0300

    fouzan

---
+++ ruby_map_parctice.rb
@@ -0,0 +1,127 @@
+students = [
+  {
+      first_name: 'Lamees',
+      last_name: 'AlFallaj'
+  },
+  {
+      first_name: 'Aisha',
+      last_name: 'AlDabbagh',
+  },
+  {
+      first_name: 'Abdulwahhab',
+      last_name: 'AlBalla',
+  }
+]
+
+# upper_case_full_names = students.map do |name|
+#     p "#{name[:first_name]}" " #{name[:last_name]}".upcase
+# end
+
+
+#       2. Find the first order for each user
+users = [
+  {
+      name: 'Fahad',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Abdulrahman',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Muhannad',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
+
+# first_order_for_each_user =  users.map do |order|
+#     p "#{order[:orders].first}"
+
+# end
+
+
+#       3. Find the average amount spent on coffee, per transaction, for each person
+people = [
+  {
+      name: 'Sarah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Saud',
+      transactions: [
+          {
+              type: 'BIKES',
+              amount: 800.00
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Norah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'COFFEE',
+              amount: 100.00
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  }
+]
+
+
+coffee_average_per_person = 
+
+people.map do |average|
+    p "#{average[:transactions:amount]}"
+end
