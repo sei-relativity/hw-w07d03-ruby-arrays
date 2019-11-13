3ae5b27d097ab73919af3d247542dbd8d9fb3a48
oalru <osamaalruthaya@hotmail.com>
2019-11-13 08:10:55 +0300

    Done and Doner

---
+++ app.rb
@@ -0,0 +1,189 @@
+students = [
+  {
+      first_name: 'Lamees',
+      last_name: 'AlFallaj'
+  },
+  {
+      first_name: 'Aisha',
+      last_name: 'AlDabbagh',
+  },
+  {
+      first_name: 'Abdulwahhab',
+      last_name: 'AlBalla',
+  }
+]
+upper_case_full_names = students.map do |full_name|
+    "#{full_name[:first_name].upcase} #{full_name[:last_name].upcase}"
+end
+puts upper_case_full_names
+
+###############
+
+users = [
+  {
+      name: 'Fahad',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Abdulrahman',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Muhannad',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
+first_order_for_each_user = users.map do |first|
+    puts "#{first[:orders].first}"
+end
+puts first_order_for_each_user
+
+###############
+
+people = [
+  {
+      name: 'Sarah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Saud',
+      transactions: [
+          {
+              type: 'BIKES',
+              amount: 800.00
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Norah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'COFFEE',
+              amount: 100.00
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  }
+]
+coffee_average_per_person = people.map do |name|
+    sum = 0
+    time = 0
+    name[:transactions].each do |transaction|
+        if transaction[:type] == "COFFEE"
+           sum += transaction[:amount]
+           time = time+1
+        end
+    end
+        {name: name[:name],
+        coffee_average: (sum/time)
+        }
+end
+puts coffee_average_per_person
+
+#############
+
+stores = [
+  {
+      store_name: 'Jarir',
+      products: [
+          {
+              description: 'Titanium',
+              price: 9384.33
+          },
+          {
+              description: 'Gold',
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: 'Tamimi',
+      products: [
+          {
+              description: 'Silver',
+              price: 654.44
+          },
+          {
+              description: 'Ruby',
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: 'Souq',
+      products: [
+          {
+              description: 'Opal',
+              price: 345.43
+          },
+          {
+              description: 'Sapphire',
+              price: 899.33
+          }
+      ]
+  }
+]
+most_expensive_products_by_store = stores.map do |product|
+    if product[:products][0][:price] > product[:products][1][:price]
+     most_expensive_product = product[:products][0]
+    else
+     most_expensive_product = product[:products][1]
+    end
+    {
+     store_name: product[:store_name],
+     most_expensive_product: most_expensive_product
+    }
+end
+   puts most_expensive_products_by_store
